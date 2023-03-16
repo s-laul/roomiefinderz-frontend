c16c3f0e220fe879e33eac2be3a318eddac9d6f0
thiagobgarc <thiagobuenogarcia1@gmail.com>
2023-03-16 19:22:22 -0400

    App.js - CHANGES - IMPORT INDEX TO LOGINPAGE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Index from './pages/Index'
+import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <h1>RoomieFinderz</h1>
-      {/* <Index/> */}
       <HomePage />
     </div>
   );
