c5c65a36b89f4710be614617a999b54f6cb959ff
thiagobgarc <thiagobuenogarcia1@gmail.com>
2023-03-16 19:18:34 -0400

    App.js - CHANGES - TO DISPLAY CONTENT

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Index from './pages/Index'
-import HomePage from './Pages/HomePage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Index/>
       <h1>RoomieFinderz</h1>
+      {/* <Index/> */}
       <HomePage />
     </div>
   );
