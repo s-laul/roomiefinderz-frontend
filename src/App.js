3f6b77cbc76e06cd4f14f7133bb98621d55c342b
slaul <sameer.laul@uconn.edu>
2023-03-16 19:31:36 -0400

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Index from './pages/Index'
-import HomePage from './pages/HomePage';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <Index/>
       <h1>RoomieFinderz</h1>
-      {/* <Index/> */}
       <HomePage />
     </div>
   );
