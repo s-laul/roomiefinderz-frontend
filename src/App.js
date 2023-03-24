32283ce77599801a37c212d18a617ab1cc78a6f0
slaul <sameer.laul@uconn.edu>
2023-03-24 12:13:55 -0400

    roommate display page and routes working

--- src/App.js
+++ src/App.js
@@ -16,15 +16,16 @@ function App() {
         <div className="App" >
         <Routes>
           <Route path='/' element={<LoginPage />} />
-               <Route path='/findroomate' element={<RoommatesDisplay />} />
-               <Route path='/createprofile' element={<CreateProfileForm />} />
-               <Route path='/postapts' element={<RoomsDisplay />} />
-               <Route path='/requestapts' element={<RoomsDisplay />} />
-               <Route path='/homepage' element={<HomePage/>} />
-               <Route path='/registration' element={<SignUp/>} />
-               <Route path='/roommatesdisplay/:name' element={<RoommateProfile />} />
+          <Route path='/findroomate' element={<RoommatesDisplay />} />
+          <Route path='/createprofile' element={<CreateProfileForm />} />
+          <Route path='/postapts' element={<RoomsDisplay />} />
+          <Route path='/requestapts' element={<RoomsDisplay />} />
+          <Route path='/homepage' element={<HomePage/>} />
+          <Route path='/registration' element={<SignUp/>} />
+          <Route path='/roommatesdisplay/:name' element={<RoommateProfile />} />
         </Routes> 
           {/* Code space */}
+
 
         </div>
           
