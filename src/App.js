e08939b56234ffd5b223a55a2f694080ac4a808e
slaul <sameer.laul@uconn.edu>
2023-03-24 12:19:07 -0400

    login and roommate display fixed

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
         <div className="App" >
         <Routes>
           <Route path='/' element={<LoginPage />} />
-          <Route path='/findroomate' element={<RoommatesDisplay />} />
+          <Route path='/roommatesdisplay' element={<RoommatesDisplay />} />
           <Route path='/createprofile' element={<CreateProfileForm />} />
           <Route path='/postapts' element={<RoomsDisplay />} />
           <Route path='/requestapts' element={<RoomsDisplay />} />
           <Route path='/homepage' element={<HomePage/>} />
           <Route path='/registration' element={<SignUp/>} />
-          <Route path='/roommatesdisplay/:name' element={<RoommateProfile />} />
+          {/* <Route path='/roommatesdisplay/:name' element={<RoommateProfile />} /> */}
         </Routes> 
           {/* Code space */}
 
