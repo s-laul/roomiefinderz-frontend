f1e4f1f8db919878c9718af8d945c7fc55700885
slaul <sameer.laul@uconn.edu>
2023-03-24 16:21:00 -0400

    roommate form and seed data updated

--- src/Components/CreateProfileForm.js
+++ src/Components/CreateProfileForm.js
@@ -36,6 +36,8 @@ const CreateProfileForm = (props) => {
          </div>
       <container className="profile-form-container">
          <form onSubmit={createProfile} className="profile-form">
+            <label name="picture">Upload Picture: &nbsp;</label>
+            <input htmlFor="picture" type="file"/><br/>
 
             <label htmlFor="name">Name: &nbsp;</label>
             <input onChange={onChangeData} value={formData.name} id="name" type="text" name="name" placeholder="First name only"/><br/>
@@ -46,11 +48,11 @@ const CreateProfileForm = (props) => {
             <label htmlFor="gender">Gender: &nbsp;</label>
             <input onChange={onChangeData} value={formData.gender} id="gender" type="text" name="gender"/><br/>
 
-            <label htmlFor="major">Major: &nbsp;</label>
-            <input onChange={onChangeData} value={formData.major} id="major" type="text" name="major"/><br/>
-
-            <label htmlFor="interests">Interests: &nbsp;</label>
-            <input onChange={onChangeData} value={formData.interests} id="interests" type="text" name="interests" placeholder="Three, separated by commas"></input><br/>
+            <label htmlFor="smoker">Smoker: &nbsp;</label>
+            <select onChange={onChangeData} value={formData.smoker} id="smoker" name="smoker">
+               <option value="no">No</option>
+               <option value="yes">Yes</option>
+            </select><br/>
 
             <label htmlFor="morningOrNight">Morning or Night Person: &nbsp;</label>
             <select onChange={onChangeData} value={formData.morningOrNight} id="morningOrNight" name="morningOrNight">
@@ -58,21 +60,19 @@ const CreateProfileForm = (props) => {
                <option value="night">Night  </option>
             </select><br/>
 
-            <label htmlFor="smoker">Smoker: &nbsp;</label>
-            <select onChange={onChangeData} value={formData.smoker} id="smoker" name="smoker">
-               <option value="no">No</option>
-               <option value="yes">Yes</option>
-            </select><br/>
-
             <label htmlFor="roommateOrRoom">Seeking Roommate or Room: &nbsp;</label>
             <select onChange={onChangeData} value={formData.roommateOrRoom} id="roommateOrRoom" name="roommateOrRoom">
                <option value="roommate">Roommate</option>
                <option value="room">Room</option>
             </select><br/>
-            
-            <label name="picture">Upload Picture: &nbsp;</label>
-            <input htmlFor="picture" type="file"/><br/><br/>
 
+            <label htmlFor="major">Major: &nbsp;</label>
+            <input onChange={onChangeData} value={formData.major} id="major" type="text" name="major"/><br/>
+
+            <label htmlFor="interests">Interests: &nbsp;</label>
+            <input onChange={onChangeData} value={formData.interests} id="interests" type="text" name="interests" placeholder="Three, separated by commas"></input><br/><br/>
+
+           
             <input type="submit" value="Submit" />
          </form>
       </container>
